0af1dc101565b746d1087cfb2ba3b777149576bb
LucasTapa <lucas.dfa@live.com>
2022-01-14 18:47:53 -0300

    requisito 4 e 10 feito

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,17 @@ localStorageSimulator('setItem');
 
 describe('4 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se saveCartItems com o argumento <ol><li>Item</li></ol> chama localStorage.setItem', () => {
+    const test = document.createElement('div');
+    test.innerHTML = '<ol><li>Item</li></ol>';
+    saveCartItems(test.firstChild);
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('Verifica se ao execitar saveCartItems com o argumento <ol><li>Item</li></ol> o metodo localStorage.setItem e chamado com dois parametros', () => {
+    const test = document.createElement('div');
+    test.innerHTML = '<ol><li>Item</li></ol>';
+    saveCartItems(test.firstChild);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', test.firstChild.innerHTML);
+  });
 });
